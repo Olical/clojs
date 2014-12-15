6c9b894ffe0994625adc3573dc2a75987a9b9414
Oliver Caldwell <olliec87@gmail.com>
2014-12-15 22:57:35 +0000

    Added plain JavaScript version.

--- fib.js
+++ fib.js
@@ -1,3 +1,14 @@
 [$if, [$mt, 10, 20],
     "Uh, well this is awkward.",
     [str, "EVERYTHING IS FINE, MOVE ALONG ", [reduce, $add, [1, 2, 3]]]]
+
+(function () {
+    if (10 > 20) {
+        return "Uh, well this is awkward.";
+    }
+    else {
+        return "EVERYTHING IS FINE, MOVE ALONG " + [1, 2, 3].reduce(function (a, b) {
+            return a + b;
+        });
+    }
+}());
