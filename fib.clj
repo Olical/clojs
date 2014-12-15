ca72fc4c44ac93792bc4125d1d095722f7173168
Oliver Caldwell <olliec87@gmail.com>
2014-12-15 22:46:12 +0000

    Initial Clojure and JavaScript syntax trees.

---
+++ fib.clj
@@ -0,0 +1,3 @@
+(if (> 10 20)
+  "Uh, well this is awkward."
+  (str "EVERYTHING IS FINE, MOVE ALONG " (reduce + [2 2 3])))
---
+++ fib.js
@@ -0,0 +1,3 @@
+[$if, [$mt, 10, 20],
+    "Uh, well this is awkward.",
+    [str, "EVERYTHING IS FINE, MOVE ALONG ", [reduce, $add, [1, 2, 3]]]]
