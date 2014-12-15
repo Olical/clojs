42d5ee814e60ff2334bd89887444d476061dc0a6
Oliver Caldwell <olliec87@gmail.com>
2014-12-15 23:29:28 +0000

    Working walk function. Wow.

--- fib.js
+++ fib.js
@@ -1,14 +1,23 @@
-[$if, [$mt, 10, 20],
-    "Uh, well this is awkward.",
-    [str, "EVERYTHING IS FINE, MOVE ALONG ", [reduce, $add, [1, 2, 3]]]]
-
-(function () {
-    if (10 > 20) {
-        return "Uh, well this is awkward.";
+function walkArrays(item) {
+    if (Array.isArray(item)) {
+        return walk(item);
     }
     else {
-        return "EVERYTHING IS FINE, MOVE ALONG " + [1, 2, 3].reduce(function (a, b) {
-            return a + b;
-        });
+        return item;
     }
-}());
+}
+
+function walk(tree) {
+    return tree[0].apply(null, tree.slice(1).map(walkArrays));
+}
+
+// var res = walk(
+//     [$if, [$mt, 10, 20],
+//         "Uh, well this is awkward.",
+//         [str, "EVERYTHING IS FINE, MOVE ALONG ", [reduce, $add, [1, 2, 3]]]]);
+
+function $mt(a, b) { return a > b; }
+function $eq(a, b) { return a === b; }
+var res = walk([$eq, false, [$mt, 10, 20]]);
+
+console.log(res);
